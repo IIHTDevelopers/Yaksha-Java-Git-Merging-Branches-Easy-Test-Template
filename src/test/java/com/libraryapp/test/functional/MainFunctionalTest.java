4aa9a16bbe01dbe9ad6346b23f83c7e00c694926
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-21 18:20:07 +0530

    Add files via upload

---
+++ src/test/java/com/libraryapp/test/functional/MainFunctionalTest.java
@@ -0,0 +1,65 @@
+package com.libraryapp.test.functional;
+
+import static com.libraryapp.test.utils.TestUtils.businessTestFile;
+import static com.libraryapp.test.utils.TestUtils.currentTest;
+import static com.libraryapp.test.utils.TestUtils.testReport;
+import static com.libraryapp.test.utils.TestUtils.yakshaAssert;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import mainapp.MyApp;
+
+public class MainFunctionalTest {
+
+	@AfterAll
+	public static void afterAll() {
+		testReport();
+	}
+
+	@Test
+	public void testDirectoryExists() throws IOException {
+		try {
+			String output = MyApp.directoryExists();
+			System.out.println(output);
+			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+		} catch (Exception ex) {
+			yakshaAssert(currentTest(), false, businessTestFile);
+		}
+	}
+
+	@Test
+	public void testAtleastOneCommit() throws IOException {
+		try {
+			String output = MyApp.atleastOneCommit();
+			System.out.println(output);
+			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+		} catch (Exception ex) {
+			yakshaAssert(currentTest(), false, businessTestFile);
+		}
+	}
+
+	@Test
+	public void testTempBranch() throws IOException {
+		try {
+			String output = MyApp.tempBranch();
+			System.out.println(output);
+			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+		} catch (Exception ex) {
+			yakshaAssert(currentTest(), false, businessTestFile);
+		}
+	}
+
+	@Test
+	public void testMergeBase() throws IOException {
+		try {
+			String output = MyApp.mergeBase();
+			System.out.println(output);
+			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+		} catch (Exception ex) {
+			yakshaAssert(currentTest(), false, businessTestFile);
+		}
+	}
+}
