ca0bc299a9615ec082c43bae81774656d2c96e31
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-19 14:28:21 +0530

    Update MainFunctionalTest.java

--- src/test/java/com/libraryapp/test/functional/MainFunctionalTest.java
+++ src/test/java/com/libraryapp/test/functional/MainFunctionalTest.java
@@ -14,52 +14,52 @@ import mainapp.MyApp;
 
 public class MainFunctionalTest {
 
-	@AfterAll
-	public static void afterAll() {
-		testReport();
-	}
+    @AfterAll
+    public static void afterAll() {
+        testReport();
+    }
 
-	@Test
-	public void testDirectoryExists() throws IOException {
-		try {
-			String output = MyApp.directoryExists();
-			System.out.println(output);
-			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
-		} catch (Exception ex) {
-			yakshaAssert(currentTest(), false, businessTestFile);
-		}
-	}
+    @Test
+    public void testDirectoryExists() throws IOException {
+        try {
+            String output = MyApp.directoryExists();
+            System.out.println(output);
+            yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+        } catch (Exception ex) {
+            yakshaAssert(currentTest(), false, businessTestFile);
+        }
+    }
 
-	@Test
-	public void testAtleastOneCommit() throws IOException {
-		try {
-			String output = MyApp.atleastOneCommit();
-			System.out.println(output);
-			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
-		} catch (Exception ex) {
-			yakshaAssert(currentTest(), false, businessTestFile);
-		}
-	}
+    @Test
+    public void testCheckCommitWithSpecificMessage() throws IOException {
+        try {
+            String output = MyApp.checkCommitWithSpecificMessage();
+            System.out.println(output);
+            yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+        } catch (Exception ex) {
+            yakshaAssert(currentTest(), false, businessTestFile);
+        }
+    }
 
-	@Test
-	public void testTempBranch() throws IOException {
-		try {
-			String output = MyApp.tempBranch();
-			System.out.println(output);
-			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
-		} catch (Exception ex) {
-			yakshaAssert(currentTest(), false, businessTestFile);
-		}
-	}
+    @Test
+    public void testFeatureBranchExists() throws IOException {
+        try {
+            String output = MyApp.featureBranchExists();
+            System.out.println(output);
+            yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+        } catch (Exception ex) {
+            yakshaAssert(currentTest(), false, businessTestFile);
+        }
+    }
 
-	@Test
-	public void testMergeBase() throws IOException {
-		try {
-			String output = MyApp.mergeBase();
-			System.out.println(output);
-			yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
-		} catch (Exception ex) {
-			yakshaAssert(currentTest(), false, businessTestFile);
-		}
-	}
+    @Test
+    public void testCheckMergeFeatureBranchWithPattern() throws IOException {
+        try {
+            String output = MyApp.checkMergeFeatureBranchWithPattern();
+            System.out.println(output);
+            yakshaAssert(currentTest(), output.equals("true"), businessTestFile);
+        } catch (Exception ex) {
+            yakshaAssert(currentTest(), false, businessTestFile);
+        }
+    }
 }
